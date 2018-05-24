a8b51ad37807ffa71b7e3a34b0964d6555dcbafb
Marc Deveaux <mdvx@users.noreply.github.com>
2018-05-24 13:29:23 -0400

    Update register.cmd

--- register.cmd
+++ register.cmd
@@ -1,6 +1,4 @@
-p:
-cd P:\crypto-rtd
 @echo Registering RTD Server
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Debug\CryptoRtd.dll /codebase
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Release\CryptoRtd.dll /codebase
-pause
+
