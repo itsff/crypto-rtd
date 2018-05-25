a68a16936037cf8971b189af0ba62658eaa4f597
Marc Deveaux <mdvx@users.noreply.github.com>
2018-05-25 19:46:54 -0400

    register.debug.cmd

--- register.debug.cmd
+++ register.debug.cmd
@@ -1,4 +1,5 @@
 @echo Registering RTD Server
+p:
+cd P:\OpenSource\Crypto\crypto-rtd
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Debug\CryptoRtd.dll /codebase
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Release\CryptoRtd.dll /codebase
 
