e37cb684bd4c68f5bc7072c0545144174cdea78f
Marc Deveaux <mdvx@users.noreply.github.com>
2018-05-25 10:07:25 -0400

    move project to crypto folder remove unused references

---
+++ register.marc.cmd
@@ -0,0 +1,4 @@
+@echo Registering RTD Server
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Debug\CryptoRtd.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\CryptoRtd\bin\Release\CryptoRtd.dll /codebase
+
