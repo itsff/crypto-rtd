979da6028c621120ecbe04916d6896dc266c9ed2
Marc Deveaux <mdvx@users.noreply.github.com>
2018-06-14 21:39:25 -0400

    trying to fix build script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,9 @@
 @echo off
 
 echo Restoring NuGet packages
-nuget\nuget.exe restore src\crypto-rtd.sln
+nuget restore src\crypto-rtd.sln
+@if ERRORLEVEL 1 pause
 
 echo Building release
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:minimal /nologo src\crypto-rtd.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release  /nologo src\CryptoRtd\CryptoRtd.csproj
+@if ERRORLEVEL 1 pause
